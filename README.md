78924a5e4fca9c69b2e10561d9d3fb5753f95b0d
Yin Guo Wei <yinguowei@gmail.com>
2018-11-10 21:39:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+
+初始化项目，创建 Spring Initializr 项目，依赖 web 
---
+++ src/main/java/cn/yinguowei/vueboot/VueBootApplication.java
@@ -0,0 +1,12 @@
+package cn.yinguowei.vueboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueBootApplication.class, args);
+    }
+}
