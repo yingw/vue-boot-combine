eb7049d7556577156cb0545ec0a6d91ed3ee6da1
Yin Guo Wei <yinguowei@gmail.com>
2019-04-25 18:42:23 +0800

    fix

--- src/main/java/cn/yinguowei/vueboot/VueBootApplication.java
+++ src/main/java/cn/yinguowei/vueboot/VueBootApplication.java
@@ -2,11 +2,25 @@ package cn.yinguowei.vueboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class VueBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VueBootApplication.class, args);
+    }
+}
+
+
+@Controller
+class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello, Vue!";
     }
 }
